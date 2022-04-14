b8916376832d1557f4274acb969bde54d2aea8d9
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-04-14 19:36:42 +0100

    added crud functionality to news

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -22,4 +22,60 @@ router.get("/", authenticateToken, async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { headline, body } = req.body;
+    const news = await pool.query(
+      "INSERT INTO news (headline, body) VALUES ($1, $2) RETURNING *",
+      [headline, body]
+    );
+    res.json({
+      message: "News created successfully",
+      news: news.rows[0],
+    });
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+router.get("/:news_id", async (req, res) => {
+  try {
+    const { news_id } = req.params;
+    const singleNews = await pool.query("SELECT * FROM news WHERE news_id=$1", [
+      news_id,
+    ]);
+    res.status(200).json({ news: singleNews.rows });
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/:news_id", async (req, res) => {
+  try {
+    const { news_id } = req.params;
+    const { headline, body } = req.body;
+    const updates = await pool.query(
+      `UPDATE news SET headline =$1, body=$2 WHERE news_id =$3`,
+      [headline, body, news_id]
+    );
+    return res.status(200).json(updates.rows);
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/:news_id", async (req, res) => {
+  try {
+    const { news_id } = req.params;
+    const deletedNews = await pool.query(`DELETE FROM news WHERE news_id =$1`, [
+      news_id,
+    ]);
+    return res.status(200).json({ message: "Deleted successfully" });
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
 module.exports = router;
