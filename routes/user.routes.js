739d9fd6d3cc6b4d2d634fc3af42f7a6312e9e71
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-04-15 17:56:19 +0100

    added email sending

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const pool = require("../db/db");
-const logger = require("../logger/logger");
-const bcrypt = require("bcrypt");
+const express = require('express');
+const pool = require('../db/db');
+const logger = require('../logger/logger');
+const bcrypt = require('bcrypt');
+const transporter = require('../mail/mailer');
 const router = express.Router();
 
 /*
@@ -11,9 +12,9 @@ const router = express.Router();
  * @returns {JSON}
  * @returns {String} message
  */
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const users = await pool.query("SELECT * FROM users");
+    const users = await pool.query('SELECT * FROM users');
     res.json({ users: users.rows });
   } catch (error) {
     logger.error(error);
@@ -28,17 +29,27 @@ router.get("/", async (req, res) => {
  * @returns {JSON}
  * @returns {String} message
  */
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const { user_name, user_password, user_email } = req.body;
+    if (!user_name || !user_password || !user_email) {
+      return res.status(400).json({ error: 'Please fill all fields' });
+    }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(user_password, salt);
     const newUser = await pool.query(
-      "INSERT INTO users (user_name, user_password, user_email) VALUES ($1, $2, $3) RETURNING *",
+      'INSERT INTO users (user_name, user_password, user_email) VALUES ($1, $2, $3) RETURNING *',
       [user_name, hashedPassword, user_email]
     );
+    let info = await transporter.sendMail({
+      from: 'drsimplegraffiti@gmail.com', // sender address
+      to: user_email, // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'You just registered on our platform', // plain text body
+      html: `You just registered on our platform your user details: ${newUser.rows[0].user_id}`, // html body
+    });
     res.json({
-      message: "User created successfully",
+      message: 'User created successfully',
       user: newUser.rows[0],
     });
   } catch (error) {
